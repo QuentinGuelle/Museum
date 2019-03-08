815b18cc3bdbc26d2c86746a6ee8073c89e84eb4
QuentinGuelle <quentin.guelle@mmibordeaux.com>
2019-03-08 09:26:54 +0100

    images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_075739) do
+ActiveRecord::Schema.define(version: 2019_03_08_081105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,22 @@ ActiveRecord::Schema.define(version: 2019_03_08_075739) do
     t.text "description"
     t.string "settings"
     t.string "season"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rarchitectures", force: :cascade do |t|
+    t.text "description"
+    t.string "link"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rblackandwhites", force: :cascade do |t|
+    t.text "description"
+    t.string "link"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
