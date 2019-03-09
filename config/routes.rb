81580d349e468f1f76a74baaf26448ca694f81df
QuentinGuelle <quentin.guelle@mmibordeaux.com>
2019-03-09 21:40:59 +0100

    add_home

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :blackandwhites
   resources :exhibitions
   resources :pictures
-  root to: "rblackandwhites#index"
+  root to: "pictures#index"
   get "/rblackandwhites", to: "rblackandwhites#index"
   get "/rarchitectures", to: "rarchitectures#index"
+  get "/pictures", to: "pictures#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
