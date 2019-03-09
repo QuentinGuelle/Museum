1835c1469e15ab49542b6604e40c6136cec32949
QuentinGuelle <quentin.guelle@mmibordeaux.com>
2019-03-09 21:17:59 +0100

    css_v2

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :exhibitions
   resources :pictures
   root to: "rblackandwhites#index"
+  get "/rblackandwhites", to: "rblackandwhites#index"
+  get "/rarchitectures", to: "rarchitectures#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
